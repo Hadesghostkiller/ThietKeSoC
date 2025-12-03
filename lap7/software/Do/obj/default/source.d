23fc8e910647301738772c35bb9bbe59ed92bd97
Hadesghostkiller <hadesghostkiller@gmail.com>
2025-12-03 10:51:11 +0700

    1

---
+++ lap7/software/Do/obj/default/source.d
@@ -0,0 +1,24 @@
+obj/default/source.o: source.c ../Do_bsp/system.h ../Do_bsp/linker.h \
+ ../Do_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../Do_bsp//HAL/inc/io.h ../Do_bsp//HAL/inc/alt_types.h \
+ ../Do_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Do_bsp//HAL/inc/sys/alt_irq.h ../Do_bsp//HAL/inc/nios2.h \
+ ../Do_bsp//HAL/inc/alt_types.h
+
+../Do_bsp/system.h:
+
+../Do_bsp/linker.h:
+
+../Do_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../Do_bsp//HAL/inc/io.h:
+
+../Do_bsp//HAL/inc/alt_types.h:
+
+../Do_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Do_bsp//HAL/inc/sys/alt_irq.h:
+
+../Do_bsp//HAL/inc/nios2.h:
+
+../Do_bsp//HAL/inc/alt_types.h:
